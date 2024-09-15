f5598be5318339354d61d0f3d06cb401b79651cd
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-09-15 11:34:57 +0100

    feat: add WeatherCard component
    - Create a basic WeatherCard component with a heading
     and imported in app.js....

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import WeatherCard from "./components/WeatherCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-col items-center mt-16">
+      <h1 className="text-3xl font-bold">Weather App</h1>
+      <WeatherCard /> 
     </div>
   );
 }
