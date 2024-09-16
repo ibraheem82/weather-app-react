9aa6a72d84a8ca00e57e61b3e058dfc814d816d9
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-09-16 03:11:36 +0100

    react icons was installed...,  feat: integrate weather data fetching in WeatherCard component
    - Add state variables to manage city input and weather data
    - Implement async function to fetch weather data from OpenWeatherMap API
    - Trigger data fetch on button click or input change using `useEffect`
    - Style the component with Tailwind CSS and add a search input with a search icon

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,5 +1,29 @@
 
 const WeatherCard = () => {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [search, setSearch] = useState(false);
+
+  const fetchWeatherData = async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}`
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log("Error fetching weather data.", error);
+      setWeatherData(null);
+      setSearch(true);
+    }
+  };
+
+  useEffect(() => {
+    if (city && search) {
+      fetchWeatherData();
+    }
+  }, [city, search]);
+
   return (
     <div>
       <h1>WeatherCard</h1>
