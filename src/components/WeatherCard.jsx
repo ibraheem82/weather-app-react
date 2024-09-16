ae506338d9b098aca9a4ac49fd62392a710f2565
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-09-16 12:49:50 +0100

    project completed .....   feat: enhance WeatherCard component with sunrise, sunset, and additional icons
    - Add sunrise and sunset times with formatted display using `HiSun` and `PiCloudSunFill` icons
    - Display temperature, wind speed, and humidity with icons and improved layout
    - Fetch weather data from OpenWeatherMap API and handle error cases
    - Include additional weather-related icons for a more informative and user-friendly UI

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react";
 import { BiWind } from "react-icons/bi";
 import { FaSearchLocation } from "react-icons/fa";
+import { HiSun } from "react-icons/hi";
 import { LuWaves } from "react-icons/lu";
+import { PiCloudSunFill } from "react-icons/pi";
 
+// Built-in API request by city name -> (API) TO USE
+const API = import.meta.env.VITE_APP_API_KEY;
 const WeatherCard = () => {
   const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState(null);
@@ -16,7 +20,7 @@ const WeatherCard = () => {
       const data = await response.json();
       console.log(data);
       setWeatherData(data);
-      setSearch(true)
+      setSearch(true);
     } catch (error) {
       console.log("Error fetching weather data.", error);
       setWeatherData(null);
@@ -41,21 +45,21 @@ const WeatherCard = () => {
           placeholder="Enter City Name"
         />
         <button onClick={fetchWeatherData}>
-        <FaSearchLocation className=""/>
-
+          <FaSearchLocation className="" />
         </button>
       </div>
 
-      {search &&
-      (
-    <div>
+      {search && (
+        <div>
           {weatherData ? (
             <div className="text-white">
               <div className="flex flex-col items-center text-5xl mt-5">
                 {(weatherData?.main?.temp - 273.15)?.toFixed(2)}°C
-
               </div>
-              <div className="flex flex-col items-center mt-4">{weatherData?.name}</div>
+              <div className="flex flex-col items-center mt-4">
+                {weatherData?.name}
+              </div>
+
               <div className="flex flex-row justify-between mt-10 text-xl">
                 <div className="flex flex-row items-center gap-x-3">
                   <BiWind />
@@ -64,21 +68,49 @@ const WeatherCard = () => {
 
                 <div className="flex flex-row items-center gap-x-3">
                   <LuWaves />
-                  <p>{(weatherData?.wind?.humidity)?.toFixed(2)} Kmph</p>
+                  <p>{weatherData?.wind?.humidity?.toFixed(2)} Kmph</p>
                 </div>
               </div>
 
+              <div className="flex flex-row justify-between mt-6 text-xl">
+                <div className="flex flex-row items-center gap-x-3">
+                  <HiSun />
+                  <p>
+                    {new Date(
+                      weatherData?.sys?.sunrise * 1000
+                    )?.toLocaleTimeString([], {
+                      // hour: "numeric",
+                      // minute: "numeric",
 
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
+                  </p>
+                </div>
 
+                <div className="flex flex-row items-center gap-x-3">
+                  <PiCloudSunFill />
+                  <p>
+                    {new Date(
+                      weatherData?.sys?.sunset * 1000
+                    )?.toLocaleTimeString([], {
+                      // hour: "numeric",
+                      // minute: "numeric",
 
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
+                  </p>
+                </div>
+              </div>
             </div>
-          ):(<div>Weather Data Not Found</div>)}
+          ) : (
+            <div>Weather Data Not Found</div>
+          )}
         </div>
-      )
-
-      }
+      )}
     </div>
   );
 };
 
-export default WeatherCard
+export default WeatherCard;
