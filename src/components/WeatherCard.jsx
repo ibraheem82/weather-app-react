f0718630a606223b3e794e7f7e835b6acc540755
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-09-16 03:51:15 +0100

    feat: display weather data in WeatherCard component
    - Add conditional rendering for weather data based on search status
    - Show temperature, city name, wind speed, and humidity if weather data is available
    - Handle case where weather data is not found
    - Style the component with Tailwind CSS for a responsive and modern layout

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -11,6 +11,8 @@ const WeatherCard = () => {
       );
       const data = await response.json();
       console.log(data);
+      setWeatherData(data);
+      setSearch(true)
     } catch (error) {
       console.log("Error fetching weather data.", error);
       setWeatherData(null);
@@ -25,10 +27,54 @@ const WeatherCard = () => {
   }, [city, search]);
 
   return (
+    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 mt-10 p-2 w-[400px] h-[320px]">
+      <div className="flex flex-row justify-between items-center bg-white rounded-2xl pl-2 pr-2">
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          className="rounded-2xl outline-none p-2"
+          placeholder="Enter City Name"
+        />
+        <button onClick={fetchWeatherData}>
+        <FaSearchLocation className=""/>
+
+        </button>
+      </div>
+
+      {search &&
+      (
     <div>
-      <h1>WeatherCard</h1>
+          {weatherData ? (
+            <div className="text-white">
+              <div className="flex flex-col items-center text-5xl mt-5">
+                {(weatherData?.main?.temp - 273.15)?.toFixed(2)}°C
+
+              </div>
+              <div className="flex flex-col items-center mt-4">{weatherData?.name}</div>
+              <div className="flex flex-row justify-between mt-10 text-xl">
+                <div className="flex flex-row items-center gap-x-3">
+                  <BiWind />
+                  <p>{(weatherData?.wind?.speed * 3.6)?.toFixed(2)} Kmph</p>
+                </div>
+
+                <div className="flex flex-row items-center gap-x-3">
+                  <LuWaves />
+                  <p>{(weatherData?.wind?.humidity)?.toFixed(2)} Kmph</p>
+                </div>
+              </div>
+
+
+
+
+            </div>
+          ):(<div>Weather Data Not Found</div>)}
+        </div>
+      )
+
+      }
     </div>
-  )
-}
+  );
+};
 
 export default WeatherCard
